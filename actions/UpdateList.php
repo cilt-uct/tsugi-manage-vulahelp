120a5f225547b1bd7f9a24e55426e8d2e96c8cac
TurRil <lightscape2001@gmail.com>
2021-12-15 15:27:20 +0200

    Update the active sites to also take the link id into account.

--- actions/UpdateList.php
+++ actions/UpdateList.php
@@ -16,7 +16,7 @@ ini_set('max_execution_time', '3600');
 
 $list = $_POST['list'];
 
-$result = $PDOX->queryDie("update {$p}vulahelp_users set active=0 where active=1;");
+$result = $PDOX->queryDie("update {$p}vulahelp_users set active=0 where link_id = :link_id and active=1;", array(':linkId' => $linkId));
 if ($result->errorCode() === '00000') {
 
     $query = "INSERT INTO {$p}vulahelp_users (`link_id`,`list`,`created_at`,`created_by`,`active`) VALUES (:linkId, :list, NOW(), :userid, 1);";
